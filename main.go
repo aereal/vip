aad55c592390761d15f63815ebc09ac4657c223d
aereal <aereal@aereal.org>
2015-11-17 19:24:47 +0900

    Command line

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
 var Version string
 
-func main() {
+var Commands = []cli.Command{
 }
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "vip"
+	app.Version = Version
+	app.Author = "aereal"
+	app.Commands = Commands
+
+	app.Run(os.Args)
+}
+
