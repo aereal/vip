3b52ce285aa449b2d069ece51a21da60cd763251
aereal <aereal@aereal.org>
2015-11-18 18:53:32 +0900

    + tests for LocalRepository

---
+++ local_repository_test.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  . "github.com/onsi/gomega"
+)
+import "testing"
+
+func TestNewLocalRepositoryIndexFromPrefix(t *testing.T) {
+  RegisterTestingT(t)
+
+  var (
+    index LocalRepositoryIndex
+    err error
+  )
+
+  index, err = NewLocalRepositoryIndexFromPrefix("test/assets/bundle/")
+  Expect(index).NotTo(BeNil())
+  Expect(index[0].Name).To(Equal("vim-plugin-1"))
+  Expect(index[0].Path).To(Equal("test/assets/bundle/vim-plugin-1"))
+  Expect(err).To(BeNil())
+}
